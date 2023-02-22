21d8a0fa40cf909b797a1bacd16ddc857dec6859
Eva González <11458423+EvaGonz-ai@users.noreply.github.com>
2023-02-22 14:37:42 +0100

    Add files via upload

---
+++ Módulo 2/ComandoGame-Módulo 2/ComandoGame/Assets/Coursera/Code/Game/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform m_PlayerTransform;
+	Vector3 m_OffsetCamera;
+	public float m_XDivider=1.0f;
+	float m_MinZCamera;
+
+	void Start()
+	{
+		m_OffsetCamera=transform.position-m_PlayerTransform.position;
+		m_MinZCamera=transform.position.z;
+	}
+	void Update()
+	{
+		Vector3 l_CameraPosition=m_PlayerTransform.position+m_OffsetCamera;
+		l_CameraPosition.x/=m_XDivider;
+		l_CameraPosition.z=Mathf.Max(m_MinZCamera, l_CameraPosition.z);
+		m_MinZCamera=l_CameraPosition.z;
+		transform.position=l_CameraPosition;
+	}
+}
---
+++ Módulo 2/ComandoGame-Módulo 2/ComandoGame/Assets/Coursera/Code/Game/GrenadeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeController : MonoBehaviour
+{
+	public GameObject m_ExplossionPrefab;
+	float m_CurrentTime=0.0f;
+	public float m_TimeToExplode=0.5f;
+
+	void Start()
+	{
+	}
+	void Update()
+	{
+		m_CurrentTime+=Time.deltaTime;
+	}
+	void OnCollisionEnter(Collision Other)
+	{
+		if(m_CurrentTime>=m_TimeToExplode)
+		{
+			GameObject l_Explossion=(GameObject)GameObject.Instantiate(m_ExplossionPrefab, transform.position, Quaternion.identity);
+			GameObject.Destroy(this.gameObject);
+			GameObject.Destroy(l_Explossion, 3.0f);
+		}
+	}
+}
